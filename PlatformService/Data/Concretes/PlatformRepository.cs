12feea654903d33f81a08f9bbf48c0378d9a353d
Isa Memmedli <i.a.memmedli@gmail.com>
2023-06-03 16:38:49 +0400

    Data Layer - Repository

---
+++ PlatformService/Data/Concretes/PlatformRepository.cs
@@ -0,0 +1,35 @@
+﻿using PlatformService.Data.Interfaces;
+using PlatformService.Models;
+
+namespace PlatformService.Data.Concretes;
+
+public class PlatformRepository : IPlatformRepository
+{
+    private readonly AppDbContext _context;
+
+    public PlatformRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async void CreatePlatform(Platform platform)
+    {
+        if(platform == null) throw new ArgumentNullException(nameof(platform));
+        await _context.Platforms.AddAsync(platform);
+    }
+
+    public IEnumerable<Platform> GetAllPlatforms()
+    {
+        return _context.Platforms.ToList();
+    }
+
+    public Platform GetPlatformById(int id)
+    {
+        return _context.Platforms.FirstOrDefault(p => p.Id == id);
+    }
+
+    public bool SaveChanges()
+    {
+        return _context.SaveChanges() >= 0;
+    }
+}
--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using PlatformService.Data;
+using PlatformService.Data.Concretes;
+using PlatformService.Data.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +14,8 @@ builder.Services.AddSwaggerGen();
 
 if (builder.Environment.IsDevelopment())
     builder.Services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("InMem"));
+
+builder.Services.AddScoped < IPlatformRepository,PlatformRepository>();
 
 var app = builder.Build();
 
