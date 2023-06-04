e15e88f7efa5d8179473b746c956784f28f3e1f4
Isa Memmedli <i.a.memmedli@gmail.com>
2023-06-04 17:57:30 +0400

    Data Layer - DB Context

---
+++ CommandsService/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CommandsService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CommandsService.Data;
+
+public class AppDbContext:DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+    {
+        
+    }
+
+    public DbSet<Platform> Platforms { get; set; }
+    public DbSet<Command> Commands { get; set; }
+}
