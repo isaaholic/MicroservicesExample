fae084b153f682534c8395d5310f6bf5a5cf278b
Isa Memmedli <i.a.memmedli@gmail.com>
2023-06-04 18:16:30 +0400

    Data Layer - Repository

---
+++ CommandsService/Data/Concretes/CommandRepository.cs
@@ -0,0 +1,47 @@
+﻿using CommandsService.Data.Interfaces;
+using CommandsService.Models;
+
+namespace CommandsService.Data.Concretes;
+
+public class CommandRepository : ICommandRepository
+{
+    private readonly AppDbContext _context;
+
+    public CommandRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public void CreateCommand(int platformId, Command command)
+    {
+        if (command == null) throw new ArgumentNullException(nameof(command));
+        command.PlatformId = platformId;
+
+        _context.Commands.Add(command);
+    }
+
+    public void CreatePlatform(Platform platform)
+    {
+        if(platform == null) throw new ArgumentNullException(nameof(platform));
+
+        _context.Platforms.Add(platform);
+    }
+
+    public IEnumerable<Platform> GetAllPlatforms()
+        => _context.Platforms.ToList();
+
+    public Command GetCommand(int platformId, int commandId)
+        => _context.Commands
+        .Where(c => c.PlatformId == platformId && c.Id == commandId).FirstOrDefault();
+
+    public IEnumerable<Command> GetCommandsForPlatform(int platformId)
+        => _context.Commands
+        .Where(c => c.PlatformId == platformId)
+        .OrderBy(c => c.Platform.Name);
+
+    public bool PlatformExist(int id)
+        => _context.Platforms.Any(p => p.Id == id);
+
+    public bool SaveChanges()
+        => _context.SaveChanges() >= 0;
+}
---
+++ CommandsService/Data/Interfaces/ICommandRepository.cs
@@ -0,0 +1,19 @@
+﻿using CommandsService.Models;
+
+namespace CommandsService.Data.Interfaces;
+
+public interface ICommandRepository
+{
+    bool SaveChanges();
+
+    // Platforms
+    IEnumerable<Platform> GetAllPlatforms();
+    void CreatePlatform(Platform platform);
+    bool PlatformExist(int id);
+
+    // Commands
+    IEnumerable<Command> GetCommandsForPlatform(int platformId);
+    Command GetCommand(int platformId,int commandId);
+    void CreateCommand(int platformId, Command command);
+    
+}
